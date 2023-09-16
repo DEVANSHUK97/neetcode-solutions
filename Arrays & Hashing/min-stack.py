e44ef86ba0cd9ddd0623189bdf234866c2aed998
Devanshu <devanshukhumrma@gmail.com>
2023-09-16 17:26:24 -0400

    min-stack

---
+++ Arrays & Hashing/min-stack.py
@@ -0,0 +1,40 @@
+#https://leetcode.com/problems/min-stack/
+lass MinStack:
+
+    def __init__(self):
+        self.data = []
+        self.size = 0
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.data.append(val)
+        self.size = self.size + 1
+
+        if self.min == []:
+            self.min = [val]
+        elif val <= self.min[-1]:
+            self.min.append(val)
+
+
+    def pop(self) -> None:
+        if self.min[-1] == self.data[-1]:
+            self.min.pop()
+
+        self.data.pop()
+
+        self.size = self.size - 1
+
+    def top(self) -> int:
+        return self.data[self.size - 1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
